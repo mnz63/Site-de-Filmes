a05166334afd64ed1de78a9c16d7b810a6e70e6b
mnzh63 <danilocsi88@gmail.com>
2022-08-24 12:00:12 -0300

    Create movie.model.ts

---
+++ src/movie.model.ts
@@ -0,0 +1,11 @@
+type movie = {
+    id: number;
+    title: string;	
+    subTitle: string;
+    category: string;
+    release_date: string;
+    rate: number;
+    overview: string;
+    background: string;
+    runtime: string;
+}
