40768dae24a674cb4bbc1ce6df92e4ac17276aa4
mnzh63 <danilocsi88@gmail.com>
2022-08-24 12:00:09 -0300

    Delete get.movie.js

--- src/get.movie.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('cross-fetch')
-
-function getById(id){
-    return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=ec2c4a1cbbc963178f8f2453299eb6be&language=pt-BR`).then(res=> res.json())
-}
-
-module.exports = getById
